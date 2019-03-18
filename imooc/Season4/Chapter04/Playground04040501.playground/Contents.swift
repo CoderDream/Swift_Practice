d7ef3df04d2d2feb165a2c23713dd9a44b0c3fa7
CoderDream <coderdream@gmail.com>
2019-03-18 11:17:30 +0800

    4-5 Swift是面向协议的编程语言 20190318

---
+++ imooc/Season4/Chapter04/Playground04040501.playground/Contents.swift
@@ -0,0 +1,9 @@
+// 4-5 Swift是面向协议的编程语言
+// https://www.imooc.com/video/12070
+
+import UIKit
+
+// 按下 Command 或 Ctrl 键，进入 Int 或 Array 的源代码，可以看到 Swift 2.0 时代，Int 和 Array 实现了很多协议
+var a: Int = 3
+
+var arr: Array<Int> = [1, 2, 3]
