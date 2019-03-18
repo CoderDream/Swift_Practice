6c5566388ba2b29e8d8f397d009a26f9d27a71eb
CoderDream <coderdream@gmail.com>
2019-03-18 09:07:26 +0800

    4-1 Swift扩展协议和默认实现 20190318

--- imooc/Season4/Chapter04/Playground04040101.playground/Contents.swift
+++ imooc/Season4/Chapter04/Playground04040101.playground/Contents.swift
@@ -9,4 +9,54 @@ protocol Record: CustomStringConvertible {
     func winningPercent() -> Double
 }
 
+// 扩展协议(CustomStringConvertible)的属性
+extension Record {
+    var description: String {
+        return String(format: "WINS: %d, LOSSES: %d", arguments: [wins, losses])
+    }
+    
+    func shoutWins() {
+        print("WE WINS ", wins, " TIMES")
+    }
+    
+    var gamePlayed: Int {
+        return wins + losses
+    }
+}
 
+struct BasketballRecord: Record {
+    var wins: Int
+    
+    var losses: Int
+    
+    func winningPercent() -> Double {
+        return Double(wins) / Double(gamePlayed)
+//        return Double(wins) / Double(wins + losses)
+    }
+
+}
+
+struct BaseballRecord: Record {
+    var wins: Int
+    
+    var losses: Int
+    
+    func winningPercent() -> Double {
+        return Double(wins) / Double(gamePlayed)
+        //        return Double(wins) / Double(wins + losses)
+    }
+}
+
+let teamRecord = BasketballRecord(wins: 2, losses: 10)
+print(teamRecord)
+// WINS: 2, LOSSES: 10
+teamRecord.shoutWins()
+
+// 扩展系统的协议
+extension CustomStringConvertible {
+    var descriptionWithDate: String {
+        return NSDate().description + " " + description
+    }
+}
+print(teamRecord.descriptionWithDate)
+// 2019-03-18 01:05:59 +0000 WINS: 2, LOSSES: 10
